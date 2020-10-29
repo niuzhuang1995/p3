7a1d9603c86ae8c8079148d2722d02bb0401be8f
niuzhuang1995 <2891032263@qq.com>
2020-10-29 23:41:24 +0800

    python脚本实战编码一：游戏代码 添加注释

--- hogwats_lg4/test.py
+++ hogwats_lg4/test.py
@@ -13,15 +13,16 @@ enemy_final_hp = enemy_hp - my_power
 """
 import random
 
-
+#执行的函数
 def game_numone(enemy_hp,enemy_power):
     my_hp = 1000
     my_power = 200
 
     while True:
+#循环减少血量
         my_hp = my_hp - enemy_power
         enemy_hp = enemy_hp - my_power
-
+#判断是否结束
         if my_hp<= 0 or enemy_hp <= 0:
             print("游戏结束")
             if my_hp < enemy_hp :
@@ -35,10 +36,13 @@ def game_numone(enemy_hp,enemy_power):
                 break
 
 if __name__ == '__main__':
+#使用三目运算符以及列表构造几个随机数，作为敌人血量的备选
     enemy_hpp = [i for i in range(900,910)]
-
+#随机获取列表中的一个数字作为敌人的血量
     enemy_hp = random.choice(enemy_hpp)
 
+#创建一个随机整数作为敌人的攻击力
     ememy_power = random.randint(188,201)
 
+#调用函数，并传入参数（敌人血量，敌人攻击力）
     game_numone(enemy_hp,ememy_power)
